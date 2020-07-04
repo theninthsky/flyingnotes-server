6dcd6c9b44d05daa806c9e16b52fd5dc03e292ff
Almog Gabay <theninesky@gmail.com>
2020-07-04 23:02:45 +0300

    Remove JWT redundant verification when an access token expires

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,44 +4,39 @@ import Token from '../models/Token.js'
 import { generateAccessToken, updateRefreshToken } from '../controllers/user.js'
 
 const { ACCESS_TOKEN_SECRET, CLIENT_URL = 'http://localhost:3000' } = process.env
-const TOKEN_EXPIRED_ERROR = 'TokenExpiredError'
 
 export default (req, res, next) => {
   const { Bearer: token } = req.cookies
 
   if (!token) return next()
 
-  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      if (err.name == TOKEN_EXPIRED_ERROR) {
-        jwt.verify(token, ACCESS_TOKEN_SECRET, { ignoreExpiration: true }, async (_, { userID, refreshTokenID }) => {
-          try {
-            const { expiresIn } = await Token.findById(refreshTokenID)
+  jwt.verify(token, ACCESS_TOKEN_SECRET, { ignoreExpiration: true }, async (err, { userID, refreshTokenID, exp }) => {
+    if (err) return res.clearCookie('Bearer').redirect(CLIENT_URL)
 
-            const dateExpiresIn = new Date(expiresIn)
+    if (Date.now() < exp * 1000) {
+      req.userID = userID
+      return next()
+    }
 
-            if (dateExpiresIn < new Date()) {
-              Token.findByIdAndDelete(refreshTokenID, () => {})
+    try {
+      const { expiresIn } = await Token.findById(refreshTokenID)
 
-              return res.clearCookie('Bearer').sendStatus(401)
-            }
+      const dateExpiresIn = new Date(expiresIn)
 
-            generateAccessToken(res, userID, refreshTokenID)
-            req.userID = userID
+      if (dateExpiresIn < new Date()) {
+        Token.findByIdAndDelete(refreshTokenID, () => {})
 
-            updateRefreshToken(refreshTokenID)
-
-            next()
-          } catch (err) {
-            return res.clearCookie('Bearer').redirect(CLIENT_URL)
-          }
-        })
-      } else {
         return res.clearCookie('Bearer').redirect(CLIENT_URL)
       }
-    } else {
-      req.userID = decoded.userID
+
+      generateAccessToken(res, userID, refreshTokenID)
+      req.userID = userID
+
+      updateRefreshToken(refreshTokenID)
+
       next()
+    } catch (err) {
+      return res.clearCookie('Bearer').redirect(CLIENT_URL)
     }
   })
 }
