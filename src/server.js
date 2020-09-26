437a0ef2ad896cffc58113b30881f5676f969edd
Almog Gabay <theninesky@gmail.com>
2020-09-26 21:43:34 +0300

    Refactor server initialization using top-level await

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
 import https from 'https'
 
+import { connect } from './database.js'
+
 const { PORT = 5000, SERVER_URL = 'https://localhost:5000' } = process.env
 
-import('./database.js').then(async ({ connect }) => {
-  try {
-    await connect()
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  }
+try {
+  await connect()
+} catch (err) {
+  console.error(err)
+  process.exit(1)
+}
 
-  const { default: app } = await import('./app.js')
+const { default: app } = await import('./app.js')
 
-  app.listen(+PORT, token => console.log(`${token ? 'Listening on port' : 'Failed to listen to port'} ${PORT}...`))
-})
+app.listen(+PORT, token => console.log(`${token ? 'Listening on port' : 'Failed to listen to port'} ${PORT}...`))
 
 setInterval(() => https.get(SERVER_URL), 900000) // keep Heroku app awake
 
